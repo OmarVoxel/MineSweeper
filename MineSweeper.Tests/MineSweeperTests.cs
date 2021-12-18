8aaf4e61189346fa392dc9944fb82865d71e3899
Omar Izquierdo <oizquierdo@voxelgroup.net>
2021-12-18 14:46:04 +0100

    Initial commit

---
+++ MineSweeper.Tests/MineSweeperTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MineSweeper.Tests
+{
+    public class MineSweeperTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
