572ba2a4f4ed44cce813bd7182def613e9b5d376
Omar Izquierdo <oizquierdo@voxelgroup.net>
2021-12-19 14:30:39 +0100

    test: MatrixFromMatrixIsntTheSameThanMatrixFromMineSweeper

--- MineSweeper.Tests/MineSweeperTests.cs
+++ MineSweeper.Tests/MineSweeperTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Xunit;
 
 namespace MineSweeper.Tests
@@ -5,5 +6,12 @@ namespace MineSweeper.Tests
     public class MineSweeperTests
     {
 
+        [Fact]
+        public void MatrixFromMatrixIsntTheSameThanMatrixFromMineSweeper()
+        {
+            Matrix _matrix = new Matrix(4, 4);
+            MineSweeper mineSweeper = new MineSweeper(4, 4, 2);
+            mineSweeper.Matrix.Should().NotBe(_matrix);
+        }
     }
 }
